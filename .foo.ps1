94d86aa031f8f134a7520ca06e1cdfe0434a464e
Art Malin <amalin@postfalls.gov>
2024-03-07 09:51:43 -0800

    Foo

--- .foo.ps1
+++ .foo.ps1
@@ -1,26 +1,19 @@
-# scratch file
-# ADD LOOP THROUGH ALL SC AND SITES
+##### SCRIPT
+$SiteURL = "https://postfallsidahoorg.sharepoint.com/sites/CityClerkDepartment"
+$GroupName = "Everyone except external users"     #But you assign it as a user
+$cclib = "CrowCanyonAppsLib"
+$sa = "SiteAssets"
+$sp = "SitePages"
 
-$Url = "https://postfallsidahoorg.sharepoint.com/sites/HRDept"
+#Connect to PnP Online
+Connect-PnPOnline -Url $SiteURL -interactive
 
-$Url
-Connect-PnPOnline –Url $Url -interactive
+#Break Permission Inheritance of the List, then Grant permission on list to Group
+Set-PnPList -Identity $cclib -BreakRoleInheritance -CopyRoleAssignments
+Set-PnPListPermission -Identity $cclib -AddRole "Read" -User $GroupName
 
-$web = Get-PnPWeb –Includes AppTiles
+Set-PnPList -Identity $sa -BreakRoleInheritance -CopyRoleAssignments
+Set-PnPListPermission -Identity $sa -AddRole "Read" -User $GroupName
 
-$appTiles = $web.AppTiles
-Invoke-PnPQuery
-
-foreach ($appTile in $appTiles)
-{
-    $appTitle = $appTile.AppTitle
-    $appType = $appTile.AppType
-    $appStatus = $appTile.AppStatus
-    $appSource = $appTile.AppSource
-    $isCorporateCatalogSite = $appTile.IsCorporateCatalogSite
-
-    if ($appType -eq "Instance")
-    {
-        $appTile  | select Title, LastModified
-    }
-}
+Set-PnPList -Identity $sp -BreakRoleInheritance -CopyRoleAssignments
+Set-PnPListPermission -Identity $sp -AddRole "Read" -User $GroupName
