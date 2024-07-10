bc5690a9936bcf0a9f318110496fdb4dc37fee91
MartialArt33 <34844294+MartialArt33@users.noreply.github.com>
2024-07-10 10:53:59 -0700

    syb

--- Temp/foo.ps1
+++ Temp/foo.ps1
@@ -1,4 +1,10 @@
-$AdminSiteURL = "https://postfallsidahoorg-admin.sharepoint.com"
-Connect-PnPOnline $AdminSiteURL -Interactive     # Connect to Tenant Admin
-Get-PnPHubSite     #List all hub sites
+$SiteURL = "https://postfallsidahoorg.sharepoint.com"  # Might need Admin sometimes
+Connect-PnPOnline $SiteURL -Interactive     # Connect to site
+
+Get-PnPSearchSettings     # Tenant top-level search settings
+
+Set-PnPSearchSettings -SearchScope Tenant
+Set-PnPSearchSettings -SearchScope DefaultScope
+# DefaultScope | Hub | Site | Tenant
+Set-PnPSearchSettings -Scope Web -SearchBoxPlaceholderText "Search Everything"
 
